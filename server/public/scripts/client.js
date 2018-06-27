572e7143a291ebd8b80d7fc8b88f7eafa7f90c92
Grace Kasahara <gracekasahara@gmail.com>
2018-06-27 16:56:35 -0500

    added input and button for new quotes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,7 +10,7 @@ function readyNow(){
         console.log(response);
         
         for (let i = 0; i < response.length; i++) {
-            $('#quotesList').append(`<li class="blockquote">${response[i].text}</li><footer class="blockquote-footer">${response[i].author}</footer>`);
+            $('#quotesList').append(`<p class="blockquote">${response[i].text}</p><footer class="blockquote-footer">${response[i].author}</footer>`);
         }
     }).fail(function(errorResponse){
         console.log(errorResponse);
